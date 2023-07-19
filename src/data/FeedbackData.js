89f8e80781ec1c2c36207a7b659024e134f2b213
Dylan Feldman <dylan@rightwayhealthcare.com>
2023-07-19 11:33:21 -0700

    First commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,29 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Learning a little each day adds up. Research shows that students who make learning a habit are more likely to reach their goals. Set time aside to learn and get reminders using your learning scheduler.',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Learning a little each day adds up. Research shows that students who make learning a habit are more likely to reach their goals. Set time aside to learn and get reminders using your learning scheduler.',
+  },
+  {
+    id: 3,
+    rating: 6,
+    text: 'Learning a little each day adds up. Research shows that students who make learning a habit are more likely to reach their goals. Set time aside to learn and get reminders using your learning scheduler.',
+  },
+  {
+    id: 4,
+    rating: 2,
+    text: 'Learning a little each day adds up. Research shows that students who make learning a habit are more likely to reach their goals. Set time aside to learn and get reminders using your learning scheduler.',
+  },
+  {
+    id: 5,
+    rating: 4,
+    text: 'Learning a little each day adds up. Research shows that students who make learning a habit are more likely to reach their goals. Set time aside to learn and get reminders using your learning scheduler.',
+  },
+];
+
+export default FeedbackData;
---
+++ src/shared/Button.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+function Button({ children, version, type, isDisabled }) {
+  return (
+    <button
+      type={type}
+      version={version}
+      disabled={isDisabled}
+      className={`btn btn-${version}`}
+    >
+      {children}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  version: 'primary',
+  type: 'button',
+  isDisabled: false,
+};
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  version: PropTypes.string,
+  types: PropTypes.string,
+  isDisabled: PropTypes.bool,
+};
+export default Button;
